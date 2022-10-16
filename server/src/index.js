943b76beacf7c2b6da3497b2a5de226452c25845
Sudhir Chavhan <sudhirchavhan100@gmail.com>
2022-10-16 16:05:24 +0530

    cors added into server

--- server/src/index.js
+++ server/src/index.js
@@ -4,7 +4,7 @@ const Port = process.env.PORT || 3755
 var cors = require('cors')
 const app=express();
 app.use(express.json());
-
+app.use(cors)
 
 const loginAuth=require("./controller/auth.controller.js")
 app.use("/",loginAuth)
