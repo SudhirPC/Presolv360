1622a0e80661388e76faad1ea82d5d4d421ee895
Sudhir Chavhan <sudhirchavhan100@gmail.com>
2022-10-16 16:14:49 +0530

    updated index file

--- server/src/index.js
+++ server/src/index.js
@@ -3,8 +3,9 @@ const connect=require("./configs/db.js")
 const Port = process.env.PORT || 3755
 var cors = require('cors')
 const app=express();
+app.use(cors())
 app.use(express.json());
-app.use(cors)
+
 
 const loginAuth=require("./controller/auth.controller.js")
 app.use("/",loginAuth)
@@ -14,7 +15,7 @@ app.use("/",RegisterAuth)
 const Taskadd=require("./controller/task.controller.js")
 app.use("/taskadd",Taskadd)
 
-app.use(cors())
+
 app.listen(Port,async function(){
     try {
         await connect();
